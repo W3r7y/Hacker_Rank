dc84334517905117bd9cd57cc8ad2b538bff8bb2
Ernest Vaulin <shenoby258@gmail.com>
2023-05-25 11:34:24 +0200

    5 solutions added

---
+++ Algorithms/Implementation/Picking Numbers/Solution.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<Integer> a) {
+        // Write your code here
+
+        int max = 0;
+
+        if(a.size() > 0){
+            max = 1;
+        }
+
+        for(int i = 0; i < a.size(); i++){
+            int count = 1;
+            for(int j = 0; j < a.size(); j++){
+                if(i == j){
+                    continue;
+                }
+                if((a.get(i)- a.get(j)) == 1 || a.get(i) == a.get(j)){
+                    count++;
+                }
+            }
+            if(count > max){
+                max = count;
+            }
+        }
+        return max;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.pickingNumbers(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
