821f6fa03d5c1c8adfaccf2340e0c634474075d6
Ernest Vaulin <shenoby258@gmail.com>
2023-07-02 19:29:24 +0200

    2 more solutions added

---
+++ Algorithms/Implementation/Extra Long Factorials/Solution.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'extraLongFactorials' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void extraLongFactorials(int n) {
+        // Write your code here
+
+        BigInteger num, result;
+        result = new BigInteger("1");
+        num = new BigInteger("0");
+        num = num.add(BigInteger.valueOf(n));
+
+        while(num.intValue() > 0){
+            result = result.multiply(num);
+            num = num.subtract(new BigInteger("1"));
+        }
+
+        System.out.println(result);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.extraLongFactorials(n);
+
+        bufferedReader.close();
+    }
+}
